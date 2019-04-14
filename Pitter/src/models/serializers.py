bcf2d01c623ac1121326a74a569937e91305cfc7
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-14 20:36:42 +0200

    Renamed module

--- Pitter/src/models/serializers.py
+++ Pitter/src/models/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import User
+from .app_models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password')
-        extra_field = {'password': {'write_only': True}}
+        extra_field = {'password': {'write_only': True }}
