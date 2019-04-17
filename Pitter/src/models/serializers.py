848421af2c2e8b5bc16e84337ef5efd5472571e0
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-17 15:30:00 +0200

    Deletes useless integration and adds migrations

--- Pitter/src/models/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .app_models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password')
-        extra_field = {'password': {'write_only': True }}
