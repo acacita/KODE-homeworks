2ab95ea4671f8c0e1c593933eaf742962326ac16
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-17 19:25:09 +0200

    Add some urls

--- Pitter/src/api/urls.py
+++ Pitter/src/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views.users import CreateUserAPIView, FollowUser, GetFollowersList
 from .views.obtaintoken import JWTView
 from .views.authorize import GetPublicKeyView, CheckAccess
+from .views.search import UserSearch
 
 urlpatterns = [
     path('create/', CreateUserAPIView.as_view()),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('secret_key/', GetPublicKeyView.as_view()),
     path('authorize/', CheckAccess.as_view()),
     path('subscribe/<slug:me>/<slug:pk>/', FollowUser.as_view()),
-    path('subscribe/<slug:person>', GetFollowersList.as_view())
+    path('subscribe/<slug:person>', GetFollowersList.as_view()),
+    path('search/<slug:person>', UserSearch.as_view())
 ]
