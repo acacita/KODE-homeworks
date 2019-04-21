e76236a834f2f3cd958de11d3b96437372fedc55
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-22 00:33:57 +0200

    Serialized an output of users list

--- Pitter/src/api/views/subscribers.py
+++ Pitter/src/api/views/subscribers.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from ..models.user_models import User, Subscribers
+from ..models.user_models import User
+from ..models.subscriber_model import Subscribers
+from ..models.user_serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import APIException
 from django.core.exceptions import ValidationError
-from ..templates.tasks import post_signup_welcome_email
+from ..templates.tasks import post_subscription_email
+
 
 # todo rewrite this using models fields after I figure out how to authorize people
 class FollowUser(APIView):
@@ -23,7 +26,7 @@ class FollowUser(APIView):
             else:
                 follow = Subscribers.objects.create(user_id=user_id, follower_id=follower)
                 follow.save()
-                post_signup_welcome_email.delay(str(me), str(pk))
+                post_subscription_email.delay(str(me), str(pk))
                 dets = {'message': 'User {} successfully subscribed to user {}'.format(str(me), str(pk))}
                 return Response(dets)
         except User.DoesNotExist:
@@ -35,7 +38,7 @@ class FollowUser(APIView):
         follow = Subscribers.objects.get(user_id=follower, follower_id=followed)
         if follow is not None:
             follow.delete()
-            return Response({'message':'You unfollowed {}'.format(pk)}, status=204)
+            return Response({'message': 'You unfollowed {}'.format(pk)}, status=204)
         else:
             raise APIException('Follow relationship does not exists')
 
@@ -45,6 +48,7 @@ class GetFollowersList(APIView):
 
     def get(self, request, person):
         user = User.objects.get(username=person)
-        data = str(user.get_connections())
-        #todo serialize this
-        return Response(data, status=200)
+        followers = user.get_connections()
+        data = UserSerializer(followers, many=True)
+
+        return Response(data.data, status=200)
