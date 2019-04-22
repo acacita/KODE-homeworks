7665a82a2fbb2a0f348ebcb1f4933f9517cdad55
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-22 02:27:19 +0200

    Added a template for future email notifications

--- Pitter/src/api/templates/tasks.py
+++ Pitter/src/api/templates/tasks.py
@@ -7,13 +7,26 @@ import logging
 
 
 @shared_task()
-def post_signup_welcome_email(user_id=None, subscribers_id=None):
+def post_subscription_email(user_id=None, subscribers_id=None):
     try:
         user = User.objects.get(username=user_id)
-
         send_mail(
             'Subscription notification',
             'You are now subscribed to {}'.format(subscribers_id),
+            EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+        )
+    except User.DoesNotExist:
+        logging.warning('Tried to send verification email to non-existing user {}'.format(user_id))
+
+@shared_task()
+def send_pitt_notification(user_id, subscribers_id):
+    try:
+        user = User.objects.get(username=user_id)
+        send_mail(
+            'New Pitt Notification',
+            'Your friend {} posted a new pitt'.format(subscribers_id),
             EMAIL_HOST_USER,
             [user.email],
             fail_silently=False,
