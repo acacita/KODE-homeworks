41800240c9f10b658bab223d3ba4a22da582bf5e
Maria Belozerova <mariebelozerova@gmail.com>
2019-04-22 02:28:06 +0200

    Changed a static folder name

--- Pitter/src/api/models/message_model.py
+++ Pitter/src/api/models/message_model.py
@@ -4,8 +4,7 @@ from django.utils import timezone
 
 
 class Message(models.Model):
-    audio_content = models.FileField(upload_to='uploads/')
+    audio_content = models.FileField(upload_to='static')
     text_content = models.TextField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
-
